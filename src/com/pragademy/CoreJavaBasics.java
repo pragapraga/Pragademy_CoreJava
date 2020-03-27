d8463bc2ac2a2b3faa23d7eaae6796eb2c57a43e
Pragadeeswaran <pragadees24@gmail.com>
2020-03-27 15:29:13 +0530

    Added new print line

--- src/com/pragademy/CoreJavaBasics.java
+++ src/com/pragademy/CoreJavaBasics.java
@@ -4,6 +4,7 @@ public class CoreJavaBasics {
 
 	public static void main(String[] args) {
 		System.out.print("Welcome to Pragademy!!!");
+		System.out.println("Lets Fi8 Against Coronoa 2019");
 	}
 
 }
