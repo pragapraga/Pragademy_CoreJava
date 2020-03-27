35886bef3460c252056623376121102c459ab85b
Pragadeeswaran <pragadees24@gmail.com>
2020-03-27 15:15:49 +0530

    Adding Exclamation

--- src/com/pragademy/CoreJavaBasics.java
+++ src/com/pragademy/CoreJavaBasics.java
@@ -3,7 +3,7 @@ package com.pragademy;
 public class CoreJavaBasics {
 
 	public static void main(String[] args) {
-		System.out.print("Welcome to Pragademy!!");
+		System.out.print("Welcome to Pragademy!!!");
 	}
 
 }
